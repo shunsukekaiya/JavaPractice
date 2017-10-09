0764c5200ab31797565ca8a1b2aa29ee57a64ba4
shunsukekaiya <dia.shunsuke@gmail.com>
2017-10-10 00:03:44 +0900

    mainの中を変更、1章

--- src/JavaPractice/main.java
+++ src/JavaPractice/main.java
@@ -3,6 +3,21 @@ package JavaPractice;
 public class main {
 
 	public static void main(String[] args) {
+		System.out.println("すがわら");
+		System.out.println("31歳です");
+		System.out.println("お酒が好きです");
+		System.out.println("31+31の計算をします");
+		System.out.println(31+ 31);
+		int x=6;
+		System.out.println(x * x *3.14);
+
+		int kensAge = 22;
+		System.out.println("ケンの年齢は" +kensAge);
+
+		kensAge = 23;
+		System.out.println("えっと、本当の年齢は" +kensAge +"です");
+
+
 		/// TODO 自動生成されたメソッド・スタブ
 
 	}
